a4cd0acb6eebb96b2cb65b6b5f06abaecbc49b5d
Petrizz <pedrogonzagamendesoficial@gmail.com>
2022-03-15 01:47:01 -0300

    JS implantado

--- registrar.php
+++ registrar.php
@@ -3,45 +3,25 @@
     $final = $_POST['final'];
     $dataHora = $_POST['dataHora'];
 
-    $ihora = intval(substr($inicio,0, -3));
+    $ihora = intval(substr($inicio, 0, -3));
     $imin = intval(substr($inicio, 3));
     $fhora = intval(substr($final, 0, -3));
-    $fmin = intval(substr($final, 3)); 
+    $fmin = intval(substr($final, 3));
+
+    $totalmin = 0;
+    $totalhora = $fhora - $ihora;
 
 
-    
+    if ($imin > $fmin) {
+        $totalmin = 60 - $imin;
+        $totalmin = $totalmin + $fmin;
+        $totalhora = $totalhora - 1;
+    }
+    if ($imin < $fmin) {
+        $totalmin = $fmin - $imin;
+    }
 
-
-/*	
-27 1540	2000 4 20
-28 1650 2000 3 10
-29 1645 1900 2 15
-30 1710 2120 4 10
-31 1450 1850 4 0
-1  1450 1950 5 0
-2  1505 1910 4 5
-3  1530 1900 3 30
-4  1435 1815 3 40
-5  1520 1950 4 30
-6  1750 2030 2 40
-7  1440 1810 3 30    */
-
-
-$totalmin = 0;
-$totalhora = $fhora - $ihora;
-
-
-if($imin > $fmin){
-    $totalmin = 60 - $imin;
-   $totalmin = $totalmin + $fmin;
-   $totalhora = $totalhora - 1;
-
-}
-if($imin<$fmin){
-    $totalmin = $fmin - $imin;
-}
-
-echo("Horas jogadas: " . $totalhora . "\nMinutos jogados: " . $totalmin);
+    echo ("Horas jogadas: " . $totalhora . "\nMinutos jogados: " . $totalmin);
 
 
     $conexao = new PDO('mysql:host=localhost; dbname=devedor', 'root', '');
@@ -58,8 +38,8 @@ echo("Horas jogadas: " . $totalhora . "\nMinutos jogados: " . $totalmin);
     $sentenca->bindValue(':final', $final);
     $sentenca->bindValue(':totalHoras', $totalhora);
     $sentenca->bindValue(':totalMin', $totalmin);
-    
-    $sentenca->execute(); 
+
+    $sentenca->execute();
 
     $conexao = null;
 
--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-alert("ola")
+function calcular() {
+
+    var botao = window.document.querySelector('div#botao')
+    var inicio = window.document.querySelector('input#inicio')
+    var final = window.document.querySelector('input#final')
+    var dataHora = window.document.querySelector('input#dataHora')
+
+
+    if (inicio.value != "" || final.value != "") { //varificando valores null
+
+        var fhora = Number(final.value.substr(0, 2))//peguei apenas a hora
+        var fmin = Number(final.value.substr(3))//peguei apenas os minutos
+
+        var ihora = Number(inicio.value.substr(0, 2))//peguei apenas a hora
+        var imin = Number(inicio.value.substr(3))//peguei apenas os minutos
+
+        var totalhora = fhora - ihora
+        var totalmin
+
+
+        //não precisa calcular qdo for igual pq os minutos totais vao ser 0
+        if (imin > fmin) {
+            totalmin = 60 - imin
+            totalmin = totalmin + fmin
+            totalhora = totalhora - 1
+        } else if (imin < fmin) {
+            totalmin = fmin - imin
+        }
+
+        if (totalhora > 0) { //verificando se a pessoa preencheu horario final como se tivesse voltado no passado
+
+            var confirm = window.confirm(`Hoje você jogou ${totalhora} horas e ${totalmin} minutos\n Suas informações estão corretas?`);
+
+            if (confirm == true) {
+
+                document.querySelector('div#calcular').remove()
+
+                botao.innerHTML = '<div class="col-md-6"><button type="submit" class="btn btn-success float-right">Salvar</button></div>'
+
+            }
+
+        }else {
+            alert(`Você não consegue voltar no tempo.\n ${final.value} vem antes de ${inicio.value}\nCorrige e calcula denovo!`)
+        }
+        
+    }else {
+        alert("Preencha os campos corretamente!!")
+    }
+}
